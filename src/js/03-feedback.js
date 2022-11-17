6416bd9f1e3f123f87191f3546ca527aaedc5ea4
Svetlana210 <svetik.may24@gmail.com>
2022-11-17 10:51:08 +0100

    add t3

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -6,29 +6,17 @@ const refs = {
     message: document.querySelector('textarea'),
 }
 
-
-
 refs.form.addEventListener('input', throttle(onFormInput, 500));
 refs.form.addEventListener('submit', onFormSubmit);
 
-
-
-
 function onFormInput(e) {
-    // e.preventDefault();
-    // const input = {
-    //     email: refs.form.elements.email.value,
-    //     message: refs.form.elements.message.value,
-    // };
    const input = {
         email: refs.email.value,
         message: refs.message.value,
     };
-    // console.log(e.target.form.elements.email.value);
-    // console.log(e.target.form.elements.message.value)
-  localStorage.setItem('feedback-form-state', JSON.stringify(input));
+    // console.log(input)
 
-    
+  localStorage.setItem('feedback-form-state', JSON.stringify(input));
 };
 
   function onFormSubmit(e) {
@@ -40,7 +28,6 @@ function onFormInput(e) {
     refs.form.reset();
     localStorage.removeItem('feedback-form-state');
   }
-
 
 const storage = localStorage.getItem('feedback-form-state');
 const parseStorage = JSON.parse(storage);
